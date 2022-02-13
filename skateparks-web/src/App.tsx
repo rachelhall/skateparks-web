a25b17b7e55b5588776b3947eb94bd197e572239
rachelhall <thharbin@gmail.com>
2022-02-13 07:53:08 -0800

    first test connecting to the backend

--- skateparks-web/src/App.tsx
+++ skateparks-web/src/App.tsx
@@ -1,9 +1,30 @@
 import React from "react";
 import "./App.scss";
+import client from "./graphql/client";
 
 import { useRouteRenderer } from "./utils/useRouteRenderer";
+import { gql } from "@apollo/client";
 
-const App = () => {
+export const App = () => {
+  client
+    .query({
+      query: gql`
+        query Query {
+          listPosts {
+            posts {
+              id
+              title
+              description
+              created_at
+            }
+            success
+            errors
+          }
+        }
+      `,
+    })
+    .then((result) => console.log(result));
+
   const currentView = useRouteRenderer();
   return <main role="main">{currentView}</main>;
 };
--- skateparks-web/src/pages/AnonHomeView/AnonHomeView.tsx
+++ skateparks-web/src/pages/AnonHomeView/AnonHomeView.tsx
@@ -1,15 +1,60 @@
 import React from "react";
 
 import "./AnonHomeView.scss";
+import { gql, useMutation } from "@apollo/client";
 
 interface IProps {}
 
 export const AnonHomeView: React.FC<IProps> = (props) => {
   const {} = props;
 
+  const CREATE_POST_METHOD = gql`
+    mutation createPark(
+      $title: string
+      $description: string
+      $created_at: string
+    ) {
+      createPost(
+        title: $title
+        description: $description
+        created_at: $created_at
+      ) {
+        success
+        errors
+        post {
+          id
+          title
+          description
+          created_at
+        }
+      }
+    }
+  `;
+
+  const [createPark] = useMutation(CREATE_POST_METHOD);
+
+  const handleCreateStandardPark = (
+    title: string,
+    description: string,
+    created_at: string
+  ) => {
+    createPark({ variables: { title, description, created_at } });
+  };
+
   return (
     <div className="AnonHomeView">
-      Anonymous home view. Setup navigation or change url to /app/login
+      <p>Anonymous home view. Setup navigation or change url to /app/login</p>
+      <button
+        onClick={() =>
+          handleCreateStandardPark(
+            "Two Rivers",
+            "Concrete park in Nashville",
+            `${Date.now()}`
+          )
+        }
+      >
+        create standard park
+      </button>
     </div>
   );
 };
